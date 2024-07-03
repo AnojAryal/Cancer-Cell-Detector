4bae979c19707f5b46113d81bae2c12495dc5d80
AnojAryal <anoj1810@gmail.com>
2024-07-03 12:52:28 +0545

    database setup

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/anojaryal/Cancer-Cell-Detector/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()	
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello! this is the strarting point of the app",
+		})
+	})
+	r.Run()
+}
