cf316f3010b6d2076f9de4b613729b5b5022457c
AnojAryal <anoj1810@gmail.com>
2024-07-03 12:57:36 +0545

    branch managed

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello! this is the strarting point of the app",
+			"message": "Hello! project",
 		})
 	})
 	r.Run()
