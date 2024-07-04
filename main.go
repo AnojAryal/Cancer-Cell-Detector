6f2f22d18e224bc655f54db381de23bccdd87c13
AnojAryal <anoj1810@gmail.com>
2024-07-05 03:01:45 +0545

    email sender added

--- main.go
+++ main.go
@@ -7,20 +7,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
 func init() {
 	initializers.LoadEnvVariables()
-	initializers.ConnectToDB()	
+	initializers.ConnectToDB()
 	initializers.SyncDatabase()
 }
 
 func main() {
 	r := gin.Default()
 	r.POST("/signup", controllers.SignUp)
-	r.POST("/login",controllers.Login)
-	r.GET("/validate",middleware.RequireAuth, controllers.Validate)
+	r.POST("/login", controllers.Login)
+	r.GET("/validate", middleware.RequireAuth, controllers.Validate)
 	r.POST("/hospitals", controllers.CreateHospital)
 	r.GET("/hospitals", controllers.GetHospitals)
-	
-	r.Run() 
+
+	r.Run()
 }
