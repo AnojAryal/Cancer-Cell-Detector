33953260325b7837f88a8f83515d79b0d9b67491
AnojAryal <anoj1810@gmail.com>
2024-07-12 12:19:58 +0545

    POST and GET api for patients

---
+++ routes/patientRoute.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/anojaryal/Cancer-Cell-Detector/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func PatientRoutes(r *gin.Engine) {
+
+	r.POST("/hospital/:hospital_id/patient", controllers.CreatePatient)
+	r.GET("/hospital/:hospital_id/patients", controllers.GetPatients)
+
+}
