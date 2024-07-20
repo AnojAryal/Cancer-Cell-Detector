bd10ff98c715a3509d9978e09f616e94c49d3901
AnojAryal <anoj1810@gmail.com>
2024-07-20 16:50:04 +0545

    permission based logic added to update cell test

--- routes/celltestRoute.go
+++ routes/celltestRoute.go
@@ -10,13 +10,13 @@ func CellTestRoutes(r *gin.Engine) {
 
 	r.POST("/hospital/:hospital_id/patients/:patient_id/cell_tests", middleware.RequireAuth, controllers.CreateCellTest)
 	r.GET("/hospital/:hospital_id/patients/:patient_id/cell_tests", middleware.RequireAuth, controllers.GetCellTests)
-	r.PUT("/hospital/:hospital_id/patients/:patient_id/cell_tests/:celltest_id", controllers.UpdateCelltest)
-	r.DELETE("/hospital/:hospital_id/patients/:patient_id/cell_tests/:celltest_id", controllers.DeleteCellTest)
-	r.POST("/hospital/:hospital_id/patients/:patient_id/cell_tests/:celltest_id/data_images", controllers.PostImageData)
-	r.GET("/hospital/:hospital_id/patients/:patient_id/cell_tests/:celltest_id/data_images", controllers.GetImageData)
-	r.POST("/hospital/:hospital_id/patients/:patient_id/cell_tests/:celltest_id/results", controllers.PostResult)
-	r.GET("/hospital/:hospital_id/patients/:patient_id/cell_tests/:celltest_id/results", controllers.GetResult)
-	r.POST("/hospital/:hospital_id/patients/:patient_id/cell_tests/:celltest_id/results/:result_id/result_images", controllers.PostResultImage)
-	r.GET("/hospital/:hospital_id/patients/:patient_id/cell_tests/:celltest_id/results/:result_id/result_images", controllers.GetResultImage)
+	r.PUT("/hospital/:hospital_id/patients/:patient_id/cell_tests/:cell_test_id", middleware.RequireAuth, controllers.UpdateCellTest)
+	r.DELETE("/hospital/:hospital_id/patients/:patient_id/cell_tests/:cell_test_id", controllers.DeleteCellTest)
+	r.POST("/hospital/:hospital_id/patients/:patient_id/cell_tests/:cell_test_id/data_images", controllers.PostImageData)
+	r.GET("/hospital/:hospital_id/patients/:patient_id/cell_tests/:cell_test_id/data_images", controllers.GetImageData)
+	r.POST("/hospital/:hospital_id/patients/:patient_id/cell_tests/:cell_test_id/results", controllers.PostResult)
+	r.GET("/hospital/:hospital_id/patients/:patient_id/cell_tests/:cell_test_id/results", controllers.GetResult)
+	r.POST("/hospital/:hospital_id/patients/:patient_id/cell_tests/:cell_test_id/results/:result_id/result_images", controllers.PostResultImage)
+	r.GET("/hospital/:hospital_id/patients/:patient_id/cell_tests/:cell_test_id/results/:result_id/result_images", controllers.GetResultImage)
 
 }
