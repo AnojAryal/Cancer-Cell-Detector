8cf745115f7c691b395d80ea426a3a48a9614c63
AnojAryal <anoj1810@gmail.com>
2024-07-20 15:58:49 +0545

    permission based logic added to post and get celltest

--- routes/celltestRoute.go
+++ routes/celltestRoute.go
@@ -2,13 +2,14 @@ package routes
 
 import (
 	"github.com/anojaryal/Cancer-Cell-Detector/controllers"
+	"github.com/anojaryal/Cancer-Cell-Detector/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func CellTestRoutes(r *gin.Engine) {
 
-	r.POST("/hospital/:hospital_id/patients/:patient_id/cell_tests", controllers.CreateCellTest)
-	r.GET("/hospital/:hospital_id/patients/:patient_id/cell_tests", controllers.GetCellTests)
+	r.POST("/hospital/:hospital_id/patients/:patient_id/cell_tests", middleware.RequireAuth, controllers.CreateCellTest)
+	r.GET("/hospital/:hospital_id/patients/:patient_id/cell_tests", middleware.RequireAuth, controllers.GetCellTests)
 	r.PUT("/hospital/:hospital_id/patients/:patient_id/cell_tests/:celltest_id", controllers.UpdateCelltest)
 	r.DELETE("/hospital/:hospital_id/patients/:patient_id/cell_tests/:celltest_id", controllers.DeleteCellTest)
 	r.POST("/hospital/:hospital_id/patients/:patient_id/cell_tests/:celltest_id/data_images", controllers.PostImageData)
